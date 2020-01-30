d00551b1dda29719f86495151d0d6e124cbd6422
Nick Iden <iden.nick@gmail.com>
2020-01-30 12:56:31 -0500

    Window set up using blank activities, there were errors so I removed the  line xmlns:app="http://schemas.android.com/apk/res-auto". I don't think it is needed for page 2 and 3 but it's just a note

---
+++ app/src/main/java/com/dainglis/trip_planner/Page3_Info_Activity.java
@@ -0,0 +1,13 @@
+package com.dainglis.trip_planner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Page3_Info_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page3__info_);
+    }
+}
