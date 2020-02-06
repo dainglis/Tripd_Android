8f31f78c4a81d43158cb83cd0516772eaa0f870e
dainglis <david.inglis@live.com>
2020-02-06 13:03:06 -0500

    Hamburger menu moved to InfoPage

--- app/src/main/java/com/dainglis/trip_planner/ActivityInfoPage.java
+++ app/src/main/java/com/dainglis/trip_planner/ActivityInfoPage.java
@@ -4,8 +4,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -75,6 +75,32 @@ public class ActivityInfoPage extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_info_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_edit_trip) {
+            return true;
+        }
+
+        if (id == R.id.action_add_event) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private Trip getCurrentTrip(long id) {
