6602c71798646174401f9daeb9ef505b99aab37d
Nick Iden <iden.nick@gmail.com>
2020-02-29 21:30:29 -0500

    adding getView() to the findViewById method in fragment. Still issues with fragment

--- a01_trip_planner/app/src/main/java/com/dainglis/trip_planner/views/TripFormFragment.java
+++ a01_trip_planner/app/src/main/java/com/dainglis/trip_planner/views/TripFormFragment.java
@@ -37,9 +37,35 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
+/* SOURCE FILE HEADER COMMENT ======================================================================
+
+    FILENAME:       TripFormFragment.java
+    PROJECT:        PROG3150 - Assignment 01
+    PROGRAMMERS:    David Inglis, Nick Iden, Steven Knapp, Michel Gomes Lima, Megan Bradshaw
+    DATE:           February 8th, 2020
+    DESCRIPTION:    The SetupPage Fregment presents the fields required to create a trip/
+                    This file contains the classes and methods used to save a trip and save it
+                    to the database.
+
+================================================================================================= */
+
 
 
 public class TripFormFragment extends Fragment {
+
+    // Create vars for cancel and confirm buttons
+    Button CanButt;
+    Button BtnConfirm;
+
+    // Create vars for edit texts
+    EditText EditName;
+    EditText EditStartCity;
+    EditText EditEndCity;
+    EditText DateDepartEnter;
+    EditText DateArriveEnter;
+    public long currentTripId = 0;
+    Bundle extras;
+    public long currentTripId = 0;
 
 
     public TripFormFragment() {
@@ -49,10 +75,293 @@ public class TripFormFragment extends Fragment {
 
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trip_form, container, false);
+
+
+
+        EditName = getView().findViewById(R.id.editName);
+        EditStartCity = getView().findViewById(R.id.editStartCity);
+        EditEndCity = getView().findViewById(R.id.editEndCity);
+        DateDepartEnter = getView().findViewById(R.id.dateDepartEnter);
+        DateArriveEnter = getView().findViewById(R.id.dateArriveEnter);
+
+        // create action for cancel button
+        CanButt = getView().findViewById(R.id.buttonSetupCancel);
+
+        CanButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //create addBtn listener
+                cancelForm();
+                //openActivity();
+
+            }
+        });
+
+
+        // create action for cancel button
+        CanButt = getView().findViewById(R.id.buttonSetupCancel);
+
+        CanButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //create addBtn listener
+                cancelForm();
+                //openActivity();
+
+            }
+        });
+
+        BtnConfirm = getView().findViewById(R.id.buttonSetupConfirm); // create action for confirm button
+
+        BtnConfirm.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                //create addBtn listener
+                if( !fieldValidation(EditName) || !fieldValidation(EditStartCity) || !fieldValidation(EditEndCity)||
+                        !dateValidate(DateDepartEnter) || !dateValidate(DateArriveEnter) )
+                {
+                }else
+                    submitForm();
+
+            }
+
+        });
+
+        extras = getIntent().getExtras();
+
+        if (extras == null) { }
+        else {
+
+            currentTripId = extras.getLong("tripId");
+
+            if (currentTripId != 0) {
+
+                // Query the database and update the layout in a secondary thread
+                AsyncTask.execute(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        loadTripDetails(currentTripId);
+                    }
+
+                });
+
+            }
+        }
+
+
+
+
     }
+
+
+    /* METHOD HEADER COMMENT -----------------------------------------------------------------------
+
+        Method:         cancelForm()
+        Description:    Cancels the form for creating a new Trip, telling the calling activity that
+                        it was cancelled.
+        Parameters:     void.
+        Returns:        void.
+
+    --------------------------------------------------------------------------------------------- */
+    private void cancelForm() {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+
+
+    /* METHOD HEADER COMMENT -----------------------------------------------------------------------
+
+        Method:         openActivity()
+        Description:    This method is called when the add button is pressed.
+                        When pressed, main page is called.
+        Parameters:     None.
+        Returns:        Void.
+
+    --------------------------------------------------------------------------------------------- */
+    public void openActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    /* METHOD HEADER COMMENT -----------------------------------------------------------------------
+
+        Method:         submitForm()
+        Description:    Submit the form for creating / editing a new Trip.
+                        If there are extras, it means that the form action is updating.
+                        If there are no extras, it means that the action is creation/insertion.
+        Parameters:     void.
+        Returns:        void.
+
+    --------------------------------------------------------------------------------------------- */
+    private void submitForm() {
+        String title = EditName.getText().toString();
+        String startLocation = EditStartCity.getText().toString();
+        String endLocation = EditEndCity.getText().toString();
+        String startDate = DateDepartEnter.getText().toString();
+        String endDate = DateArriveEnter.getText().toString();
+
+        final Trip trip = new Trip(title, startLocation, endLocation, startDate, endDate);
+
+        try {
+
+            if (extras == null) {
+
+                // Save the new trip to the db in a background thread,
+                // then return to the calling Activity
+                AsyncTask.execute(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        TripDatabase.getInstance(null).tripDAO().insert(trip);
+                        //      Toast.makeText(TripFormActivity.this,"Trip saved", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+
+            } else {
+
+                trip.setTripId(currentTripId);
+                AsyncTask.execute(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        TripDatabase.getInstance(null).tripDAO().update(trip);
+                        //     Toast.makeText(TripFormActivity.this,"Trip edited", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+
+            }
+
+            setResult(RESULT_OK);
+        }
+
+        // this is bad practice
+        catch (Exception e) {
+            setResult(RESULT_CANCELED);
+        }
+
+        finish();
+
+    }
+
+
+
+    /* METHOD HEADER COMMENT -----------------------------------------------------------------------
+
+        Method:         dateValidate()
+        Description:    This method is called when the add button is triggered. Validating the
+                        date entered by the user.
+        Parameters:     EditText        EditTextDate        Date string.
+        Returns:        boolean         True                If there are no exceptions thrown
+                                        False               If an exception is caught.
+
+    --------------------------------------------------------------------------------------------- */
+    public boolean dateValidate(EditText EditTextDate)
+    {
+        // set the date format
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Date ObjDate = null;
+        df.setLenient(false);
+
+        // use try catch to compare string with df format
+        try
+        {
+            ObjDate = df.parse(EditTextDate.getText().toString());
+            return true;
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(TripFormActivity.this,EditTextDate.getText().toString() + " is not a valid date.", Toast.LENGTH_LONG).show();
+            return false;
+
+        }
+    }
+
+
+
+    /* METHOD HEADER COMMENT -----------------------------------------------------------------------
+
+        Method:         fieldValidation()
+        Description:    This method is called when the add / edit button is pressed. This
+                        method validates the input fields as entered by the user
+        Parameters:     EditText        editText        The text to be verified.
+        Returns:        boolean         False           If the form is incomplete
+                                        True            If the form is complete
+
+    --------------------------------------------------------------------------------------------- */
+    public boolean fieldValidation(EditText editText)
+    {
+        if (editText.getText().toString() == null || editText.getText().toString().trim().length() < 1){
+
+            Toast.makeText(TripFormActivity.this,"Fill the complete trip information", Toast.LENGTH_SHORT).show();
+            return false;
+
+        } else {
+            return true;
+        }
+    }
+
+
+
+    /* METHOD HEADER COMMENT -----------------------------------------------------------------------
+
+        Method:         loadTripDetails()
+        Description:    If there were "Extras", given a unique ID for a Trip, the details of the
+                        Trip are retrieved from the "trip_planner" database.
+        Parameters:     long        id      The unique id of the requested Trip object.
+        Returns:        void.
+
+    --------------------------------------------------------------------------------------------- */
+    protected void loadTripDetails(long tripId) {
+
+        // Query the database for the Trip associated with tripId
+        Trip currentTrip = getCurrentTrip(tripId);
+
+        if (currentTrip != null) {
+            // Edit texts to be populated
+
+            EditName.setText(currentTrip.getTitle());
+            EditStartCity.setText(currentTrip.getStartLocation());
+            EditEndCity.setText(currentTrip.getEndLocation());
+            DateDepartEnter.setText(currentTrip.getStartDate());
+            DateArriveEnter.setText(currentTrip.getEndDate());
+
+        }
+    }
+
+
+
+    /* METHOD HEADER COMMENT -----------------------------------------------------------------------
+
+        Method:         getCurrentTrip()
+        Description:    Queries the `trip_planner` database for the Trip object with the specified
+                        id.
+        Parameters:     long        id      The unique id of the requested Trip object.
+        Returns:        Trip                The Trip object corresponding to the provided Id
+
+    --------------------------------------------------------------------------------------------- */
+    private Trip getCurrentTrip(long id) {
+        return TripDatabase.getInstance(getApplicationContext()).tripDAO().getById(id);
+    }
+
+
+
+
 }
+
