b3f36cff7035cd3da24914a7677510a4d5cd6242
Nick Iden <iden.nick@gmail.com>
2020-04-16 23:35:51 -0400

    New version of google maps fragment. Created with a check if map object is null and an initializer.
    If works the next step would be how to call it in info layout

---
+++ app/src/main/java/com/dainglis/trip_planner/views/Google_Maps_Fragment.java
@@ -0,0 +1,89 @@
+/* SOURCE FILE HEADER COMMENT ======================================================================
+
+    FILENAME:       Google_Maps_Fragment.java
+    PROJECT:        PROG3150 - Assignment 03
+    PROGRAMMERS:    David Inglis, Nick Iden, Steven Knapp, Michel Gomes Lima, Megan Bradshaw
+    DATE:           April 16th, 2020
+    DESCRIPTION:    The Google_Maps_fragment calles the GoogleMaps object and creates it in
+                    the fragment.
+
+================================================================================================= */
+
+package com.dainglis.trip_planner.views;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dainglis.trip_planner.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class Google_Maps_Fragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
+
+    // public constructor
+    public Google_Maps_Fragment() {
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_google__maps_, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle saveInstanceState){
+        super.onViewCreated(view, saveInstanceState);
+        mMapView = (MapView) mView.findViewById(R.id.map);
+        // check if mapBiew exists
+        if(mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+
+        }
+
+    }
+
+    /* METHOD HEADER COMMENT -----------------------------------------------------------------------
+        Method:         onMapReady(GoogleMap googleMap)
+        Description:    This method is called ti initialize the google map object in a fragment.
+        Parameters:     GoogleMap googleMap obj
+        Returns:        void.
+
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+
+        // set map type
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43,-80)));
+
+    }
+}
